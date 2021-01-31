66e372c2be3cc549687b3d77e7322a23293a96d4
Codiniqw <locondeable12@gmail.com>
2021-01-30 18:38:08 -0600

    cambio

--- interfaz.py
+++ interfaz.py
@@ -1,6 +1,5 @@
 import tkinter
 import PySimpleGUI as sg 
-import pandas as pd
 
 
 class Interfaz:
@@ -12,15 +11,13 @@ class Interfaz:
             [sg.Table(
                 headings = None,
                 key='table1',  
-                values=[["1","2","4"],["1","2","4"]],
-                
-                max_col_width=25,
+                values=[["HOLA","hOLA2","hOLA"],["1","2","4"],["1","2","4"],["1","2","4"]],
+                max_col_width=50,
                 auto_size_columns=False,
                 justification='left',
                 background_color='#85C1E9', 
                 num_rows=5,
                 enable_events=True)],
-                #[pd.DataFrame({"Foo": (1, 2, 3, 4), "Bar": (7, 13, 17, 19)})],
                 [sg.Text("Edicion de libros")],
                 [sg.Text("ISBN"), sg.Input(size=(20,2))],
                 [sg.Text("Titulo"), sg.Input(size=(20,2))],
