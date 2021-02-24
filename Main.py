5be931b0f3214317527ac435738c2cf5dfc4edaf
Codiniqw <locondeable12@gmail.com>
2021-02-24 01:01:48 -0600

    Fixed

--- Main.py
+++ Main.py
@@ -2,5 +2,5 @@ from interfaz import Interfaz
 
 
 if __name__ == "__main__":
-    Interfaz.interfaz(Interfaz)
-    Interfaz.addLibro(Interfaz,12335,'5 Semanas en globo','Julio Verne','Ciencia ficcion',25)
+    Interfaz.interfaz()
+    Interfaz.addLibro(12335,'5 Semanas en globo','Julio Verne','Ciencia ficcion',25)
