cedff6871653390110e60f04e8a9639f5573eb3d
Codiniqw <locondeable12@gmail.com>
2021-02-26 22:21:12 -0600

    Tabla dinamica funcionando

--- Main.py
+++ Main.py
@@ -3,5 +3,5 @@ from interfaz import Interfaz
 
 if __name__ == "__main__":
     Interfaz.interfaz()
-    #Interfaz.addLibro(123258,'Cancion de hielo y fuego','George R Martin','ficcion medieval',3)
+    #Interfaz.addLibro(5,'Cancion de hielo y fuego','George R Martin','ficcion medieval',3)
     #Interfaz.mostrarDatos()
--- prueba.py
+++ prueba.py
@@ -1,36 +1,52 @@
 import PySimpleGUI as sg
 import random
 import string
+from MongoConect import Conector
+import pymongo
 
 """
     Basic use of the Table Element
 """
 
-sg.theme('Dark Blue')
-
+sg.theme('Dark Blue6')
 # ------ Some functions to help generate data for the table ------
 def word():
     return ''.join(random.choice(string.ascii_lowercase) for i in range(10))
 def number(max_val=1000):
     return random.randint(0, max_val)
 
-def make_table(num_rows, num_cols):
-    data = [[j for j in range(num_cols)] for i in range(num_rows)]
-    data[0] = [word() for __ in range(num_cols)]
-    for i in range(1, num_rows):
-        data[i] = [word(), *[number() for i in range(num_cols - 1)]]
+    
+def make_table(num_rows):
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    mydb = myclient["biblioteca"]
+    mycol = mydb["libros"]
+    data = [[j for j in range(5)] for i in range(num_rows+1)] #Define el tamaño de la tabla 5 columas, n filas +1 porque en el [se gurda el encabezado]
+    data[0] = ["ISBN","Titulo", "Autor","Genero","Cantidad"] #Se Define el emcabezodo o titulos en el espacio[0]
+    for i in range(1, num_rows+1):# ciclo que rella la filas   
+        myquery = { "isbn": i }
+        for documento in mycol.find(myquery):
+            query = {
+                    "isbn": str(documento["isbn"]),
+                    "titulo": str(documento["titulo"]),
+                    "autor": str(documento["autor"]),
+                    "genero": str(documento["genero"]),
+                    "cantidad": str(documento["cantidad"]),
+            }
+        data[i] = [ query.get('isbn'),query.get('titulo'),query.get('autor'),query.get('genero'),query.get('cantidad')] #array que almacena rellena las columnas
     return data
-
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["biblioteca"]
+mycol = mydb["libros"]
 # ------ Make the Table Data ------
-data = make_table(num_rows=5, num_cols=5)
+data = make_table(num_rows=mycol.count())
 headings = [str(data[0][x])+'     ..' for x in range(len(data[0]))]
 
 # ------ Window Layout ------
 layout = [[sg.Table(values=data[1:][:], headings=headings, max_col_width=25,
                     # background_color='light blue',
                     auto_size_columns=True,
-                    display_row_numbers=True,
-                    justification='right',
+                    display_row_numbers=False,
+                         justification='right',
                     num_rows=10,
                     alternating_row_color='lightyellow',
                     key='-TABLE-',
